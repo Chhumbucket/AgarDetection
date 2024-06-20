e87c1818ab12cd55be2f6a016d44692d4e8a6f00
Chhumbucket <dylchhum@gmail.com>
2024-06-20 15:48:16 -0700

    Added Contours

--- RefineEdge.py
+++ RefineEdge.py
@@ -11,6 +11,8 @@ img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 equalizedImg = cv.equalizeHist(img_gray)
 blurredImage = cv.GaussianBlur(equalizedImg, (5,5), 0)
 
+
+
 edges = cv.Canny(image=blurredImage, threshold1=30, threshold2=150)
 kernel = cv.getStructuringElement(cv.MORPH_RECT, (2, 2))
 edges_cleaned = cv.morphologyEx(edges, cv.MORPH_CLOSE, kernel)
@@ -18,9 +20,16 @@ edges_cleaned = cv.morphologyEx(edges, cv.MORPH_CLOSE, kernel)
 
 # Canny Edge Detection
 # Display Canny Edge Detection Image
+contours, hierarchy = cv.findContours(edges_cleaned, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+cnt = contours[4]
+cv.drawContours(img, contours , -1, (0,255,0), 3)
+cv.imshow('Edge', img)
+cv.waitKey(0)
 cv.imshow('Before Morph', edges)
 cv.waitKey(0)
 cv.imshow('Canny Edge Detection', edges_cleaned)
 cv.waitKey(0)
 cv.destroyAllWindows()
 
+#Since the image is transparancent, maybe lets do it where if it is slightly associated with the color
+#We color it in 
